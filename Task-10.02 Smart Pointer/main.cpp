93b16d70a92420addd09c86d4a57904a3c0cc45d
Margaret Alade <alade.margaret@yandex.ru>
2024-02-27 05:53:09 +0300

    Initial commit

--- Task-10.02 Smart Pointer/main.cpp
+++ Task-10.02 Smart Pointer/main.cpp
@@ -6,9 +6,30 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <memory>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class node
+{
+public:
+    int m_value;
+    std::weak_ptr<node> parent;
+    node(int value) : m_value{ value } {};
+    ~node() { std::cout << "destructor called\n"; }
+};
+
+int main()
+{
+    {
+        auto node1 = std::make_shared<node>(1);
+        auto node2 = std::make_shared<node>(2);
+        node1->parent = node2;
+        node2->parent = node1;
+        
+        std::cout << "Получаем доступ к полям:\n";
+        node1->parent.lock()->m_value = 10;
+        std::cout << "m_value: " << node1->parent.lock()->m_value << "\n";
+    }
+
     return 0;
 }
